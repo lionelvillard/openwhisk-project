894297ccf71d807d91fbfdfd241747bdf54b30cf
Lionel Villard <villard@us.ibm.com>
2017-06-26 14:54:10 -0400

    Add 'sequence' action handler (#18).

--- libs/deployer/test/sequences/sequence.js
+++ libs/deployer/test/sequences/sequence.js
@@ -64,7 +64,56 @@ test('deploy-sequence1', async t => {
         basePath: 'test/sequences/fixtures',
         cache: t.context.tmpdir,
         location: 'manifest.yaml',
-        force:true
+        force: true
     })
     diff.deepEqualModulo(t, result, sequenceGold)
+})
+
+const sequenceNoSugarGold = {
+    packages: [{
+        qname: 'sequence-test2',
+        deployResult: {
+            name: 'sequence-test2',
+            binding: {},
+            publish: false,
+            annotations: [],
+            version: '0.0.4',
+            parameters: [],
+            namespace: 'org_openwhisk-deployer-test-space'
+        }
+    }],
+    actions: [{
+        qname: 'mysequence1',
+        location: '',
+        kind: 'sequence',
+        params: [],
+        deployResult: {
+            name: 'mysequence1',
+            publish: false,
+            annotations: [{key: 'exec', value: 'sequence'}],
+            version: '0.0.3',
+            exec: {
+                kind: 'sequence',
+                components: ['/whisk.system/utils/echo', '/whisk.system/utils/cat']
+            },
+            parameters: [{
+                key: '_actions',
+                value: ['/whisk.system/utils/echo', '/whisk.system/utils/cat']
+            }],
+            limits: {timeout: 60000, memory: 256, logs: 10},
+            namespace: 'org_openwhisk-deployer-test-space'
+        }
+    }]
+}
+
+test('sequence-nosugar', async t => {
+    const result = await deployer.deploy(t.context.bx.ow, {
+        basePath: 'test/sequences/fixtures',
+        cache: t.context.tmpdir,
+        location: 'manifest-nosugar.yaml',
+        force: true
+    })
+    // console.log(util.inspect(result, {depth: null}))
+
+    diff.deepEqualModulo(t, result, sequenceNoSugarGold)
 })
