75af2f9118e154bf7e37bcd074ff0777170a81a7
Lionel Villard <villard@us.ibm.com>
2017-06-28 12:02:21 -0400

    Add forward and retry combinators

--- libs/deployer/plugins/actions/combinator/parser.js
+++ libs/deployer/plugins/actions/combinator/parser.js
@@ -18,49 +18,32 @@ const chevrotain = require('chevrotain')
 // ----------------- lexer -----------------
 
 const createToken = chevrotain.createToken
-const tokenMatcher = chevrotain.tokenMatcher
 const Lexer = chevrotain.Lexer
 const Parser = chevrotain.Parser
 
-const ActionLiteral = createToken({
-    name: 'ActionLiteral',
-    pattern: /[\w@.-]+/
-})
-
-const IfToken = createToken({
-    name: 'IfToken',
-    pattern: /if/
-})
-
-const ThenToken = createToken({
-    name: 'ThenToken',
-    pattern: /then/
-})
-
-const TryToken = createToken({
-    name: 'TryToken',
-    pattern: /try/,
-    longer_alt: ActionLiteral
-})
-
-const CatchToken = createToken({
-    name: 'CatchToken',
-    pattern: /catch/
-})
-
-const RetryToken = createToken({
-    name: 'RetryToken',
-    pattern: /retry/
-})
-
-const ForwardToken = createToken({
-    name: 'ForwardToken',
-    pattern: /forward/
-})
+const ActionLiteral = createToken({name: 'ActionLiteral', pattern: /[\w@.-]+/})
+const IfToken = createToken({name: 'IfToken', pattern: /if/})
+const ThenToken = createToken({name: 'ThenToken', pattern: /then/})
+const TryToken = createToken({name: 'TryToken', pattern: /try/, longer_alt: ActionLiteral})
+const CatchToken = createToken({name: 'CatchToken', pattern: /catch/})
+const RetryToken = createToken({name: 'RetryToken', pattern: /retry/})
+const ForwardToken = createToken({name: 'ForwardToken', pattern: /forward/})
+const AfterToken = createToken({name: 'AfterToken', pattern: /after/})
+const WithToken = createToken({name: 'WithToken', pattern: /with/})
+const TimesToken = createToken({name: 'TimesToken', pattern: /times/})
+const LSquare = createToken({name: 'LSquare', pattern: /\[/})
+const RSquare = createToken({name: 'RSquare', pattern: /]/})
+const Comma = createToken({name: 'Comma', pattern: /,/})
+const IntegerLiteral = createToken({name: "IntegerLiteral", pattern: /\d+/})
 
 const QuotedActionLiteral = createToken({
     name: 'QuotedActionLiteral',
     pattern: /'[\w@.-][\w@ .-]*[\w@.-]'+/
+})
+
+const StringLiteral = createToken({
+    name: 'StringLiteral',
+    pattern: /"(?:[^\\"]|\\(?:[bfnrtv"\\/]|u[0-9a-fA-F]{4}))*"/
 })
 
 const WhiteSpace = createToken({
@@ -77,12 +60,25 @@ const allTokens = [
     CatchToken,
     RetryToken,
     ForwardToken,
+    AfterToken,
+    WithToken,
+    TimesToken,
+    LSquare,
+    RSquare,
+    Comma,
+
+    IntegerLiteral,
     ActionLiteral,
-    QuotedActionLiteral
+    QuotedActionLiteral,
+    StringLiteral
 ]
 const DeployLexer = new Lexer(allTokens)
 
 // ----------------- parser -----------------
+
+const TrimQuotes = str => {
+    return str.substr(1, str.length - 2)
+}
 
 function DeployParser(input) {
     Parser.call(this, input, allTokens)
@@ -96,16 +92,16 @@ function DeployParser(input) {
                     return $.SUBRULE($.eca)
                 }
             },
-            // {
-            //     ALT: () => {
-            //         return $.SUBRULE($.forwarder)
-            //     }
-            // },
-            // {
-            //     ALT: () => {
-            //         return $.SUBRULE($.retry)
-            //     }
-            // },
+            {
+                ALT: () => {
+                    return $.SUBRULE($.forwarder)
+                }
+            },
+            {
+                ALT: () => {
+                    return $.SUBRULE($.retry)
+                }
+            },
             {
                 ALT: () => {
                     return $.SUBRULE($.trycatch)
@@ -148,6 +144,59 @@ function DeployParser(input) {
         }
     })
 
+    $.RULE('forwarder', () => {
+        $.CONSUME(ForwardToken)
+        const $forward = $.SUBRULE($.arrayofstrings)
+        $.CONSUME(AfterToken)
+        const $actionName = $.CONSUME(ActionLiteral).image
+        $.CONSUME(WithToken)
+        const $actionArgs = $.SUBRULE2($.arrayofstrings)
+
+        return {
+            action: {
+                copy: '/whisk.system/combinators/forwarder',
+                inputs: {
+                    $forward,
+                    $actionName,
+                    $actionArgs
+                }
+            }
+        }
+    })
+
+    $.RULE('retry', () => {
+        $.CONSUME(RetryToken)
+        const $actionName = $.CONSUME(ActionLiteral).image
+        const $attempts = parseInt($.CONSUME(IntegerLiteral).image)
+        $.OPTION(() => {
+            $.CONSUME(TimesToken)
+        })
+        return {
+            action: {
+                copy: '/whisk.system/combinators/retry',
+                inputs: {
+                    $actionName,
+                    $attempts
+                }
+            }
+        }
+    })
+
+    $.RULE('arrayofstrings', () => {
+        const strings = []
+        $.CONSUME(LSquare)
+        $.OPTION(() => {
+            strings.push(TrimQuotes($.CONSUME1(StringLiteral).image))
+
+            $.MANY(() => {
+                $.CONSUME2(Comma)
+                strings.push(TrimQuotes($.CONSUME3(StringLiteral).image))
+            })
+        })
+        $.CONSUME4(RSquare)
+        return strings
+    })
+
     Parser.performSelfAnalysis(this)
 }
 
@@ -163,17 +212,16 @@ module.exports = {
     getEntities: context => {
         // 1. Tokenize the input.
         const lexResult = DeployLexer.tokenize(context.action.combinator)
+        if (lexResult.errors.length > 0)
+            throw lexResult.errors
 
         // 2. Parse the Tokens vector.
         parser.input = lexResult.tokens
         const value = parser.combinators()
+        if (parser.errors.length > 0)
+            throw parser.errors
 
         value.actionName = context.actionName
         return [value]
-        // return {
-        //     value: value,
-        //     lexResult: lexResult,
-        //     parseErrors: parser.errors
-        // }
     }
 }
