e5937026f0b80217fbec3ab592652774e51421ee
Lionel Villard <villard@us.ibm.com>
2017-06-28 21:42:16 -0400

    Regen package-lock and move npm install into link.sh

--- libs/link.sh
+++ libs/link.sh
@@ -1,9 +1,10 @@
 #!/usr/bin/env bash
 
 (cd utils && npm link)
-(cd builder && npm link)
+(cd builder && npm link && npm link @openwhisk-deploy/utils)
 
-(cd builder && npm link @openwhisk-deploy/utils)
-
+(cd deployer && npm install)
 (cd deployer && npm link @openwhisk-deploy/utils)
 (cd deployer && npm link @openwhisk-deploy/builder)
+
+(cd deployer/plugins/actions/combinator && npm install)
