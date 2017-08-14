2c77a4a6402a38d4c51292548a453c8b1c21d305
Lionel Villard <villard@us.ibm.com>
2017-08-14 13:26:19 -0400

    update runtest script to fail if one of the tests fails

--- bin/runtests.sh
+++ bin/runtests.sh
@@ -6,10 +6,12 @@ export BX_SPACE_CI=openwhisk-deploy-ci`uuidgen`
 
 bx iam space-create ${BX_SPACE_CI}
 
-#echo Wait 10 seconds
-#sleep 10 # to make sure the OW keys gets initialized
-
 (cd ${DIR}/.. && npm test)
+code=$?
+
 (cd ${DIR}/../plugins/actions/combinator && npm test)
+(( code = code || $? ))
 
 bx iam space-delete ${BX_SPACE_CI} -f
+
+exit $code
