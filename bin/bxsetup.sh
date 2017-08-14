013b0bc228a9f56efc14e78fe4e13f65bb6b996f
Lionel Villard <villard@us.ibm.com>
2017-08-14 11:54:13 -0400

    fix travis: download latest bx cli

--- bin/bxsetup.sh
+++ bin/bxsetup.sh
@@ -2,8 +2,8 @@
 
 if ! type bx > /dev/null; then
     # Install the Bluemix CLI for testing on the IBM cloud
-    wget https://public.dhe.ibm.com/cloud/bluemix/cli/bluemix-cli/Bluemix_CLI_0.5.4_amd64.tar.gz && \
-    tar zxvf Bluemix_CLI_0.5.4_amd64.tar.gz && \
+    wget https://public.dhe.ibm.com/cloud/bluemix/cli/bluemix-cli/latest/Bluemix_CLI_amd64.tar.gz && \
+    tar zxvf Bluemix_CLI_amd64.tar.gz && \
     cd Bluemix_CLI && \
     sudo ./install_bluemix_cli
 fi
