8e93f6a9416af567f5b551ae4663ba2222dd12fd
Lionel Villard <villard@us.ibm.com>
2017-08-30 16:16:32 -0400

    disable running tests in bluemix

--- bin/runtests.sh
+++ bin/runtests.sh
@@ -2,19 +2,19 @@
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
-SPACE=${BX_SPACE:-openwhisk-deploy-ci`uuidgen`}
+# SPACE=${BX_SPACE:-openwhisk-deploy-ci`uuidgen`}
 
-bx iam space-create ${SPACE}
+# bx iam space-create ${SPACE}
 
-echo Wait 20s for key to sync
-sleep 20
+# echo Wait 20s for key to sync
+# sleep 20
 
 (cd ${DIR}/.. && npm test)
 code=$?
 
-(cd ${DIR}/../plugins/actions/combinator && npm test)
-(( code = code || $? ))
+#(cd ${DIR}/../plugins/actions/combinator && npm test)
+#(( code = code || $? ))
 
-bx iam space-delete ${SPACE} -f
+# bx iam space-delete ${SPACE} -f
 
 exit $code
